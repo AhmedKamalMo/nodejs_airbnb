b62141b4d9498a6893040884823c8b96da9c675a
Ahmed Kamal <57872185+AhmedKamalMo@users.noreply.github.com>
2025-05-18 02:36:46 +0300

    get review by bookingId

--- controller/Reviews.js
+++ controller/Reviews.js
@@ -49,9 +49,9 @@ exports.createReview = async (req, res) => {
       return res.status(404).json({ message: "Hotel not found" });
     }
 
-    hotel.reviews.push({ reviewId: review._id }); 
+    hotel.reviews.push({ reviewId: review._id });
     await hotel.save();
-    
+
     res.status(201).json({ message: "Review created successfully", review });
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -89,3 +89,14 @@ exports.deleteReviewById = async (req, res) => {
 
 
 };
+exports.bookingReviewById = async (req, res) => {
+  try {
+    const reviews = await Review.find({ bookingId: req.params.id });
+    if (!reviews) {
+      return res.status(404).json({ message: "No reviews found for this booking." });
+    }
+    res.status(200).json(reviews);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error });
+  }
+}
